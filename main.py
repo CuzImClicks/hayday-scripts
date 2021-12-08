f4fa8dfeb25479f375e97930455a56175a3065bd
Clicks <58398364+CuzImClicks@users.noreply.github.com>
2021-12-08 10:05:16 +0100

    added more crops

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from Crops import Crop
 
 if __name__ == "__main__":
     wheat = Crop("wheat", 2.0, 3.6)
-    soy = Crop("soy beans", 20.0, 14)
+    soy = Crop("soy beans", 20.0, 10.8)
+    mais = Crop("mais", 5, 7)
+    sugar_cane= Crop("sugar cane", 30, 14)
+    carrot = Crop("carrot", 10, 7.2)
+
     print(wheat.getSellPerMinute())
     print(soy.getSellPerMinute())
+    print(mais.getSellPerMinute())
+    print(sugar_cane.getSellPerMinute())
+    print(carrot.getSellPerMinute())
