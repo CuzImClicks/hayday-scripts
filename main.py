06cb57b31dcbb7025dec4a7892a500681563a4f0
Clicks <58398364+CuzImClicks@users.noreply.github.com>
2021-12-09 09:11:29 +0100

    added indigo

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from Crops import Crop
 
 if __name__ == "__main__":
     wheat = Crop("wheat", 2.0, 3.6)
-    soy = Crop("soy beans", 20.0, 10.8)
     mais = Crop("mais", 5, 7)
-    sugar_cane= Crop("sugar cane", 30, 14)
     carrot = Crop("carrot", 10, 7.2)
+    soy = Crop("soy beans", 20.0, 10.8)
+    sugar_cane= Crop("sugar cane", 30, 14)
+    indigo = Crop("indigo", 120, 25)
 
     print(wheat.getSellPerMinute())
     print(soy.getSellPerMinute())
     print(mais.getSellPerMinute())
     print(sugar_cane.getSellPerMinute())
     print(carrot.getSellPerMinute())
+    print(indigo.getSellPerMinute())
