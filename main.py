dc0959d62db9bacdd6c9f969c3c719d5226661f2
Clicks <58398364+CuzImClicks@users.noreply.github.com>
2021-12-09 12:00:43 +0100

    added pumpkin

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from Crops import Crop
 if __name__ == "__main__":
     wheat = Crop("wheat", 2.0, 3.6)
     mais = Crop("mais", 5, 7)
-    carrot = Crop("carrot", 10, 7.2)
     soy = Crop("soy beans", 20.0, 10.8)
     sugar_cane= Crop("sugar cane", 30, 14)
+    carrot = Crop("carrot", 10, 7.2)
     indigo = Crop("indigo", 120, 25)
+    pumpkin = Crop("pumpkin", 180, 32)
 
     array = []
     array.append(wheat.toDict())
@@ -15,6 +16,7 @@ if __name__ == "__main__":
     array.append(soy.toDict())
     array.append(sugar_cane.toDict())
     array.append(indigo.toDict())
+    array.append(pumpkin.toDict())
     
     value = "time"
 
